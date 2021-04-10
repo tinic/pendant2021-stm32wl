e097f3a1ecb11a6b51da5ca013a111a2bb91953b
Tinic Uro <turo@adobe.com>
2021-04-10 05:26:00 -0700

    Enable build error.

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+export PATH="/opt/gcc-arm-none-eabi-10-2020-q4-major/bin:${PATH}"
+set -e
+build_type="Ninja"
+
 rm -rf build_debug
 mkdir -p build_debug
 cd build_debug
 cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug ..
 ninja
 cd ..
+
 rm -rf build_release
 mkdir -p build_release
 cd build_release
