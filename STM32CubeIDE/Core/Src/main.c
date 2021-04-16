08c4c549a3e1055c39903823ba25f8f029898c09
Tinic Uro <tinicuro@gmail.com>
2021-04-16 12:25:09 -0700

    Fix build.

--- STM32CubeIDE/Core/Src/main.c
+++ STM32CubeIDE/Core/Src/main.c
@@ -23,6 +23,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <errno.h>
+#include <sys/stat.h>
 #ifdef USER_APP_BUILD
 #include "user_app.h"
 #endif  // #ifdef USER_APP_BUILD
@@ -84,6 +86,37 @@ int __io_putchar(int ch) {
 int __io_getchar(void) {
        return 0;
 }
+
+static uint8_t *__sbrk_heap_end = NULL;
+
+__attribute__((used)) void *_sbrk(ptrdiff_t incr)
+{
+  extern uint8_t _end; /* Symbol defined in the linker script */
+  extern uint8_t _estack; /* Symbol defined in the linker script */
+  extern uint32_t _Min_Stack_Size; /* Symbol defined in the linker script */
+  const uint32_t stack_limit = (uint32_t)&_estack - (uint32_t)&_Min_Stack_Size;
+  const uint8_t *max_heap = (uint8_t *)stack_limit;
+  uint8_t *prev_heap_end;
+
+  /* Initialize heap end at first call */
+  if (NULL == __sbrk_heap_end)
+  {
+    __sbrk_heap_end = &_end;
+  }
+
+  /* Protect heap from growing into the reserved MSP stack */
+  if (__sbrk_heap_end + incr > max_heap)
+  {
+    errno = ENOMEM;
+    return (void *)-1;
+  }
+
+  prev_heap_end = __sbrk_heap_end;
+  __sbrk_heap_end += incr;
+
+  return (void *)prev_heap_end;
+}
+
 /* USER CODE END 0 */
 
 /**
