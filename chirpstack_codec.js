325664c4097d8e9fcb2d8c2dd0259c082bcd6898
Tinic Uro <tinicuro@gmail.com>
2021-04-19 04:17:23 -0700

    Add chirpstack decoder.

---
+++ chirpstack_codec.js
@@ -0,0 +1,49 @@
+// Decode decodes an array of bytes into an object.
+//  - fPort contains the LoRaWAN fPort number
+//  - bytes is an array of bytes, e.g. [225, 230, 255, 0]
+//  - variables contains the device variables e.g. {"calibration": "3.5"} (both the key / value are of type string)
+// The function must return an object, e.g. {"temperature": 22.5}
+
+function u82f(v, min, max) {
+    return ((v * ( 1.0 / 255.0 ) * (max - min) ) + min);
+}
+
+function Decode(fPort, bytes, variables) {
+  if (fPort == 1) {
+    var systemTime = (bytes[0]<<0) | (bytes[1]<<8);
+    var effectN = bytes[2];
+    var temperature = u82f(bytes[3], 0.0, 50.0);
+    var humidity = u82f(bytes[4], 0.0, 1.0);
+    var batteryVoltage = u82f(bytes[5], 2.7, 4.2);
+    return {
+      "systemTime" : systemTime,
+      "effectN" : effectN,
+      "temperature" : temperature,
+      "humidity" : humidity,
+      "batteryVoltage" : batteryVoltage
+    };
+  }
+  if (fPort == 2) {
+    var systemTime = (bytes[0]<<0) | (bytes[1]<<8);
+    var effectN = bytes[2];
+    var brightness = u82f(bytes[3], 0.0, 1.0);
+    var batteryVoltage = u82f(bytes[4], 2.7, 4.2);
+    var systemVoltage = u82f(bytes[5], 2.7, 4.2);
+    var vbusVoltage = u82f(bytes[6], 0.0, 5.5);
+    var chargeCurrent = u82f(bytes[7], 0.0, 1000.0);
+    var temperature = u82f(bytes[8], 0.0, 50.0);
+    var humidity = u82f(bytes[9], 0.0, 1.0);
+    return {
+      "systemTime" : systemTime,
+      "effectN" : effectN,
+      "brightness" : brightness,
+      "batteryVoltage" : batteryVoltage,
+      "systemVoltage" : systemVoltage,
+      "vbusVoltage" : vbusVoltage,
+      "chargeCurrent" : chargeCurrent,
+      "temperature" : temperature,
+      "humidity" : humidity
+    };
+  }
+  return {};
+}
